5a5327881f330b919382ac0ab32b782b7c11e4ac
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-10 14:57:47 -0700

    Finished MVP. Added return statement to fetching data rendering condition on CharacterListView.js

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -6,9 +6,9 @@ import { CharacterList } from "../components";
 import { getCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
 
   componentDidMount() {
     // call our action
@@ -18,10 +18,12 @@ class CharacterListView extends React.Component {
   render() {
     if (this.props.isFetching) {
       // return something here to indicate that you are fetching data
-      <div className="fetching-state-container">
-        <h1 className="fetching-animation">|</h1>
-        <h2>One moment please...</h2>
-      </div>;
+      return (
+        <div className="fetching-state-container">
+          <h1 className="fetching-animation">|</h1>
+          <h2>One moment please...</h2>
+        </div>
+      );
     }
     return (
       <div className="CharactersList-wrapper">
